c6575fc596931674b05880690039db41957cf0c4
W4lly63 <giulfy@hotmail.com>
2017-08-31 10:43:46 +0200

    update 0.9.8

---
+++ Exileserver/addon/a3_WY_Wrecks/config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches {
+	class RSZ {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.36;
+		requiredAddons[] = {};
+	};
+};
+
+class CfgFunctions {
+	class RSZF {
+		class WYWrecks {
+			file = "x\addons\a3_WY_Wrecks";
+			class init {
+				preInit = 1;
+			};
+		};
+	};
+};
